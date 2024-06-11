2a635a6701b42c83396d8e3ce07228641a9fe58c
Dainc23 <ncd2331999@gmail.com>
2024-06-11 18:11:33 +0700

    <41>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AppProvider } from './AppContext';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About_us from './Pages/about_us/About_us';
 import Contact from './Pages/contact/Contact';
